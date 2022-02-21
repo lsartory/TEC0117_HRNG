e962a9f9f30ba8f303bceb17d11cf92c92d3d50f
lsartory <lambert.sartory@gmail.com>
2022-02-21 21:45:56 +0100

    Instantiate a PLL to improve clock stability

--- src/TEC0117_HRNG.vhd
+++ src/TEC0117_HRNG.vhd
@@ -7,28 +7,38 @@ use ieee.numeric_std.all;
 entity TEC0117_HRNG is
 	port (
 		CLK_100MHz : in  std_logic;
-        CLRn       : in  std_logic;
-		LED        : out std_logic_vector(7 downto 0)
+        USER_BTN   : in  std_logic;
+		LED        : out std_logic_vector(8 downto 1)
 	);
 end entity;
 
 --------------------------------------------------
 
 architecture TEC0117_HRNG_arch of TEC0117_HRNG is
+    signal pll_clk  : std_logic := '0';
+    signal clrn     : std_logic := '1';
+
     signal clock_divider : unsigned(23 downto 0) := (others => '0');
     signal led_output    : unsigned( 7 downto 0) := (others => '0');
 begin
 
-	process (CLK_100MHz)
+    main_pll: entity work.Main_PLL
+    port map (
+        clkout => pll_clk,
+        clkin  => CLK_100MHz
+    );
+    clrn <= USER_BTN;
+
+	process (pll_clk)
 	begin
-		if rising_edge(CLK_100MHz) then
+		if rising_edge(pll_clk) then
             clock_divider <= clock_divider + 1;
             if clock_divider = 9999999 then
                 clock_divider <= (others => '0');
                 led_output    <= led_output + 1;
             end if;
 
-            if CLRn = '0' then
+            if clrn = '0' then
                 clock_divider <= (others => '0');
                 led_output    <= (others => '0');
             end if;
